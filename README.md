192d547cd2b428a648811a3764167643666a49db
blestonbandeira <blestonbandeira@gmail.com>
2019-11-13 17:13:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hjxcvg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hjxcvg)
---
+++ src/app/form.ts
@@ -0,0 +1,25 @@
+export class Form {
+  id_form: number;
+  id_interviwe: number;
+  interviewDate: Date;
+  applicantName: string;
+  firstOptionCourse: string;
+  preferencesAprendizagem: string;
+  preferencesTodos: string;
+  objectives: string;
+  description: string;
+  rules: string;
+  family: string;
+  familyUnemployement: string;
+  hobbies: string;
+  reasons: string;
+  presentation: string;
+  posture: string;
+  pauses: string;
+  speech: string;
+  understanding: string;
+  observations: string;
+  result: string;
+  finalSay: string;
+
+}
